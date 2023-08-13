7354d7d4276c774674cd77f2b41e79445b063557
William Roy <wroy@proton.me>
2023-08-13 18:20:00 -0400

    Fix flickering from unavailable dri driver

--- base.nix
+++ base.nix
@@ -35,4 +35,24 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
+
+  # Backport mutter patch which fix flickering in scenarios where the dri
+  # driver is unavailable.
+  #
+  #   https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3117
+  #
+  nixpkgs.overlays = [
+    (self: super: {
+      gnome = super.gnome.overrideScope' (gself: gsuper: {
+        mutter = gsuper.mutter.overrideAttrs (old: rec {
+           patches = (old.patches or [ ]) ++ [
+            (super.fetchpatch {
+              url = "https://gitlab.gnome.org/GNOME/mutter/-/commit/626498348b96e7ebdb2ab90fb7d2b3446578333a.patch";
+              hash = "sha256-KLx0qbPwbooHEIMt0r2g/CjyWb2M5Mv3gNdA3YM6qJA";
+            })
+          ];
+        });
+      });
+    })
+  ];
 }
